4820ddd05c2fd5faf8d8198b4191d9ff05c4726d
sophieatgithub <sophrossit@gmail.com>
2021-07-07 20:45:20 +0100

    chapter 2:12 (ed3)

---
+++ calculator.py
@@ -0,0 +1,13 @@
+# calculator.py
+# A simple calculator
+# Problem: A program which solves simple sums
+# Specification: Inputs sum, outputs solution
+# Design: 1. Prompt user for first calculation. 2. Calculate solution. 3. Print solution
+
+def main():
+    cal = eval(input("enter a sum: "))
+    print(cal)
+
+
+main()
+
